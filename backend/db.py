011750fadedd5e21b971a73ac6fe8ab82479d87e
PranavPachpute15 <pranavdpachpute15@gmail.com>
2026-03-26 22:16:18 +0530

    DB connection fix

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,11 @@
 import mysql.connector
+import os
 
 def get_db_connection():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="pranav@/2005",
-        database="event_db"
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT", 3306))
     )
-    return connection
