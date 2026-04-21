a3f7a50d4da1f6119a8da50a1ee4159d15223973
PranavPachpute15 <pranavdpachpute15@gmail.com>
2026-04-21 15:37:31 +0530

    use DATABASE_URL for db connection

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,6 @@
+import os
 import psycopg2
 
 def get_db_connection():
-    return psycopg2.connect(
-        host="db.fejiwqlhufkvxlgfiblb.supabase.co",
-        database="postgres",
-        user="postgres",
-        password="pranav@8468898011",
-        port=5432
-    )
+    database_url = os.getenv("DATABASE_URL")
+    return psycopg2.connect(database_url)
